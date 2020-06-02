003cdfd01737e42fafaf057de2b278e9ea420d63
Dayo <1690328478@qq.com>
2020-06-02 20:46:39 +0800

    Add files via upload

---
+++ code/CustomizePort.java
@@ -0,0 +1,15 @@
+package com.limelight.preferences;
+
+public class CustomizePort {
+    public static int tcp_47984 = 30000;
+    public static int tcp_47989 = 30001;
+    //远程唤醒端口 默认的7和9就硬编码了
+    public static int udp_47998 = 47998;
+    public static int udp_47999 = 47999;
+    public static int udp_48000 = 48000;
+    public static int udp_48002 = 48002;
+    public static int udp_48010 = 48010;
+
+
+
+}
